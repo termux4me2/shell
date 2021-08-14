f574cfa47d4d591bc9c9cf54d9bfbc8949ddcd26
termux4me2 <87329441+termux4me2@users.noreply.github.com>
2021-08-14 22:58:37 +0100

    update

--- use.sh
+++ use.sh
@@ -1,34 +1,33 @@
 d_n="../shell"
 u="use.sh"
-if [ -f "$u" ];
-then
-echo
-else
-cp -rf shell/$u .
-fi
 restA(){
 echo -en 'same commmand used \r';sleep
 echo -en 'restarting .\r';sleep 01;echo -en 'restarting ..\r';sleep 01;echo -en 'restarting ...\r';sleep 1;
 echo -en 'restarting .\r';sleep 01;echo -en 'restarting ..\r';sleep 01;echo -en 'restarting ...\r';sleep 1
 }
-
 main(){
 git clone https://github.com/termux4me2/shell
+if [ -d "shell" ];
+then 
+#check if shell is download
 cd shell
-samE=$(diff comX comY|wc -c)
+samE=$(diff comX ../comY|wc -c)
 if [ "$samE" -eq "0" ];
 then
-bash comX
-else
 restA
+else
+bash comX
 fi
 mv comX comY
 mv comY ..
 rm -rf $d_n
+else
+echo 'shell directory not found'
+fi
 }
 
 opXvalue=0
 while [ "$opXvalue" -eq "0" ];
-then
+do
 main
-fi
+done
