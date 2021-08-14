73ae5452177f30126b55063e46a9c0733788633c
termux4me2 <87329441+termux4me2@users.noreply.github.com>
2021-08-15 00:37:55 +0100

    Update use.sh

--- use.sh
+++ use.sh
@@ -1,18 +1,21 @@
-d_n="../shell"
-u="use.sh"
+d_n="shell";u="use.sh";rx='restarting';opXvalue='0'
+resta(){
+echo -en "$rx.\r";sleep 01;echo -en "$rx ..\r";sleep 01;echo -en "$rx ...\r";sleep 1;
+}
 restA(){
-echo -en 'same commmand used \r';sleep
-echo -en 'restarting .\r';sleep 01;echo -en 'restarting ..\r';sleep 01;echo -en 'restarting ...\r';sleep 1;
-echo -en 'restarting .\r';sleep 01;echo -en 'restarting ..\r';sleep 01;echo -en 'restarting ...\r';sleep 1
+echo -en 'same commmand used \r';sleep 0.8
+resta;resta
 }
 main(){
-git clone https://github.com/termux4me2/shell
+git clone -q https://github.com/termux4me2/shell
 if [ -d "shell" ];
-then 
+then
 #check if shell is download
+if [ -f "comY" ];
+then
 cd shell
-samE=$(diff comX ../comY|wc -c)
-rm ../comY
+samE=$(diff comX ../comY|wc -c 2>/dev/null)
+rm ../comY 2>/dev/null
 if [ "$samE" -eq "0" ];
 then
 restA
@@ -21,13 +24,13 @@ bash comX
 fi
 mv comX comY
 mv comY ..
+cd ..
 rm -rf $d_n
 else
 echo 'shell directory not found'
 fi
+fi
 }
-
-opXvalue=0
 while [ "$opXvalue" -eq "0" ];
 do
 main
